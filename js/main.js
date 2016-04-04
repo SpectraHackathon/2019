a71a9c2c821989de211cd88a7bd628c053c219d0
truongmm <truongmm@gmail.com>
2016-04-03 23:51:02 -0700

    Initial Website

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function init() {
+  $('.navbar-nav li').click(function() {
+     $(".navbar-nav li").removeClass("active");
+     $(this).addClass("active");
+  });
+  initializeMap();
+}
+
+function initializeMap() {
+  var siliconValley = new google.maps.LatLng(37.3885531,-122.0766911);
+  var mapContainer = document.getElementById("map");
+  var mapOptions = {
+    center: siliconValley,
+    scrollwheel: false,
+    zoom: 11
+  };
+  var map = new google.maps.Map(mapContainer, mapOptions);
+  var marker = new google.maps.Marker({
+      map: map,
+      position: siliconValley
+    });
+}
+
+$(document).ready(init);
