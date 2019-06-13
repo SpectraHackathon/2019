c96b1c0492cc812191b3d6ade092a5f19c37946c
Lorman Lau <lormanlau@gmail.com>
2019-06-12 23:00:26 -0700

    added location area

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-77365611-1');
+
+function initMap() {
+  // The location of Uluru
+  var makeSchool = {lat: 37.787730, lng: -122.411020};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: makeSchool});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: makeSchool, map: map});
+}
